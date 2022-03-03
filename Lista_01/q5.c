bf58850d51234e10c71816a1383e0afbcd2e3ecc
Pedro Makson <maksonjp080@gmail.com>
2022-03-02 23:30:59 -0300

    QUESTÃO 05

--- Lista_01/q5.c
+++ Lista_01/q5.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-    int numero, i, soma = 0, somaQuadrados = 0;
+    int numero, i, soma = 0, somaQuadrados = 0, somaCubos = 0;
 
     printf("--> Escolha um número: ");
     scanf("%i", &numero);
 
+    printf("\n+---------------------------+\n");
+
     for(i = 1; i <= numero; i++) {
         soma += i;
-        somaQuadrados += sqrt(i);
+        somaQuadrados += i * i;
+        somaCubos += i * i * i;
     }
 
-    printf("\n--> Soma: %i\n\n", soma);
-    printf("\n--> Soma dos quadrados: %i\n\n\n\n", somaQuadrados);
+    printf("\n    Soma: %i\n", soma);
+    printf("    Soma dos quadrados: %i\n", somaQuadrados);
+    printf("    Soma dos cubos: %i\n\n\n\n", somaCubos);
 
     return (0);
 }
