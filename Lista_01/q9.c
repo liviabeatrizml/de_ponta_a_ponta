4e1769787195d3c6af49bc005d4e024a1419099e
Pedro Makson <maksonjp080@gmail.com>
2022-03-03 00:23:16 -0300

    QUESTÃO 09

---
+++ Lista_01/q9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i, j = 0, contador = 10;
+    char numero;
+
+    for (i = -1; i <= 127; i++){
+
+        if(j < 10) {
+            printf(" %c ", i);
+            j += 1;
+        }
+
+        if(j == 10) {
+            j = 0;
+            printf("\n");
+        }
+    }
+
+    printf("\n\n\n\n");
+
+    return (0);
+}
