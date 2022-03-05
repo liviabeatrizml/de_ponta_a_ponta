2915f56b0ae1a8bbf61a12b01743961c21943cad
Pedro Makson <maksonjp080@gmail.com>
2022-03-04 23:11:27 -0300

    QUESTÃO 01

---
+++ Lista_01/q1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int quantidade, i, soma = 0;
+    float media;
+
+    printf("--> Quantos números você quer ler para realizar a média?\n");
+    printf("    Digite: ");
+    scanf("%i", &quantidade);
+    system("clear");
+
+    int vetor[quantidade];
+
+    for(i = 0; i < quantidade; i++) {
+        printf("--> %iª Valor: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+    system("clear");
+
+    for(i = 0; i < quantidade; i++) {
+        if(i+1 < quantidade) {
+            printf("%i + ", vetor[i]);
+        } else {
+            printf("%i = ", vetor[i]);
+        }
+    }
+
+    for(i = 0; i < quantidade; i++) {
+        soma = soma + vetor[i];
+    }
+
+    printf("%i\n\n", soma);
+    printf("       %i / %i\n\n", soma, quantidade);
+
+    media = (float)soma/quantidade;
+
+    printf("  --> Média: %.2f\n\n\n\n", media);
+
+    return (0);
+}
