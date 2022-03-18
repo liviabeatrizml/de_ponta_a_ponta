261047a14570c54600ab337f29eb84a51c7a72b7
Livia Beatriz <liviabeatrizmaia7@gmail.com>
2022-03-17 22:10:35 -0300

    Questão 21 - alternativa A

---
+++ Lista_01/q21a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int MDC(int num, int num2);
+
+int main(){
+    int n, n2;
+
+    printf("MAXIMO DIVISOR COMUM");
+    printf("Informe dois numeros para saber seu maximo divisor comum:\n-> ");
+    scanf("%d",&n);
+    printf("-> ");
+    scanf("%d",&n2);
+    printf("\nO maximo divisor comum entre %d e %d = %d\n\n",n,n2,MDC(n,n2));
+
+    return 0;
+}
+
+int MDC(int num, int num2){
+    if(num2 == 0){
+        return num;
+    }else {
+        return MDC(num2, num % num2);
+    }
+}
